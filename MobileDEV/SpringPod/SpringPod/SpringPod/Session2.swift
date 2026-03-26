6ebae23e1e1e3846681e019c8a3556edf8ff36aa
TaqariHill2 <taqari.hill@hgs.hiddengeniusproject.org>
2026-03-25 18:08:06 -0700

    Created new Xcode Project

---
+++ MobileDEV/SpringPod/SpringPod/SpringPod/Session2.swift
@@ -0,0 +1,7 @@
+//
+//  Session2.swift
+//  SpringPod
+//
+//  Created by Taqari Hill on 3/25/26.
+//
+
---
+++ MobileDEV/SpringPod/SpringPod/SpringPod/SpringPodApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpringPodApp.swift
+//  SpringPod
+//
+//  Created by Taqari Hill on 3/25/26.
+//
+
+import SwiftUI
+
+@main
+struct SpringPodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
